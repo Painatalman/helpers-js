c5e2f94c739b62abbb62ed92254bdb7c4e327a9d
Painatalman <maiquedi@gmail.com>
2016-04-19 17:19:25 +0100

    updated description on modals2footer.js

--- utils/modals2footer.js
+++ utils/modals2footer.js
@@ -1,6 +1,7 @@
 /**
- * Modals should be on a top-level position in order to render properly. This function moves all "modals" with a default "modal" class to the bottom of the document body. However, it accepts other querySelectors.
+ * Bootstrap Modals should be on a top-level position in order to render properly. This function moves all "modals" with a default "modal" class to the bottom of the document body. However, it accepts other querySelectors.
  * @param  {String} modalQuerySelector "A query selector for modals"
+ * @param {String} modalQuerySelectorException "an optional selector to exclude from the search. Basically, you may just omit this and add :not(query) to your first parameter."
  */
 module.exports = function moveModalsToBodyEnd(modalQuerySelector, modalQuerySelectorException) {
   var 

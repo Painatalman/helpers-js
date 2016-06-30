6195e8770598d245efca223d7f0c82d998f6722f
Painatalman <maiquedi@gmail.com>
2016-06-30 17:08:52 +0100

    more changes to lipsum

--- WIP/lipsum.js
+++ WIP/lipsum.js
@@ -89,7 +89,7 @@ var lIpsum = {
             return text;
         }
     },
-    parse_spiritualist: function parse_spiritualist(spiritualist, node, title_data, subtitle_data, text_data, guarantee_data, schedule_data,address_data, contact_data)
+    parse_spiritualist: function parse_spiritualist(spiritualist, node, title_data, subtitle_data, text_data, guarantee_data, schedule_data,address_data, contact_data) {},
     soccerPlayers: {
         "name": ["miguel", "tiago", "josé", "rodrigo", "izmael"],
         "lastName": ["castro", "fava", "rosa"],
@@ -97,51 +97,89 @@ var lIpsum = {
     },
     bootlegs: ['rookman'],
     spiritualists: {
-        "rank": ['professor', 'mestre'],
-        "name": ['turé','bambo','iziaca','cadri'],
-        "speciality": [
+        "ranks": ['professor', 'mestre'],
+        "names": ['turé','bambo','iziaca','cadri','braima'],
+        "specialities": [
             'professor', 
             'espiritualista', 
             'médico', 
             'astrólogo',
             'curandeiro',
             'cientista',
-            'especialista'
+            'especialista',
+            'vidente',
+            'mágico africano'
         ],
-        "problem": [
-            "saúde",
-            "amor",
-            "negócios",
-            "união familiar",
-            "desviar e prender",
+        "problems": [
             "afastar e aproximar",
-            "pessoas amadas",
-            "exames",
+            "amor",
+            "depressão",
+            "desviar e prender",
             "doenças",
-            "vícios de alcoolismo e droga",
-            "impotência sexual",
-            "maus olhados",
-            "poder com super magia negra e branca",
-            "sobrenaturais",
-            "justiça",
+            "doenças crónicas",
             "doenças espirituais",
-            "sexo"
+            "exames",
+            "impotência sexual",
+            "justiça",
+            "limpezas",
+            "má sorte",
+            "maus olhados",
+            "negócios",
+            "pessoas amadas",
+            "poder com super magia negra e branca",
+            "protecção contra o mal",
+            "saúde",
+            "sexo",
+            "sobrenaturais",
+            "união familiar",
+            "vícios de alcoolismo e droga"
         ],
         "text": [
             "Grande espiritualista, ajuda a resolver qualquer que seja o seu caso, mesmo grave ou difícil solução, tal como: %specialities%, etc... é um dos melhores profissionais do País. O seu assunto estará resolvido muito rapidamente. Lê a sorte e faz trabalho à distância.",
-            "Dotado de conhecimentos e poderes, ajuda a resolver problemas em menos de 3 dias, difíceis ou graves, com eficácia e garantia, como: %specialities%, etc... Lê sorte, dá previsão de Vida, faz consulta à distância. Se quer prender a si uma vida nova, com segurança e pondo fim a tudo o que o/a preocupa. Contacte o %name% pois não deixe agravar os seus problemas nos casos acima mencionados.",
-            "Grande mestre das ciências ocultas, dotado de grandes poderes e conhecimentos científicos das ciências ocultas herdadas da família. %specialities%. O seu futuro depende da sua decisão de contactar o %title% e se quer uma solução honesta para os seus problemas, sejam grandes, graves ou antigos, contacte-o. Sigilo absoluto. Efectua trabalhos com %methods%. Faz trabalhos à distância. Consulta pessoalmente ou por cartas."
+            "Dotado de conhecimentos e poderes, ajuda a resolver problemas em menos de 3 dias, difíceis ou graves, com eficácia e garantia, como: %specialities%, %specialities%, %specialities%, %specialities%, %specialities%, etc... Lê sorte, dá previsão de Vida, faz consulta à distância. Se quer prender a si uma vida nova, com segurança e pondo fim a tudo o que o/a preocupa. Contacte o %name% pois não deixe agravar os seus problemas nos casos acima mencionados.",
+            "Grande mestre das ciências ocultas, dotado de grandes poderes e conhecimentos científicos das ciências ocultas herdadas da família. %specialities%. O seu futuro depende da sua decisão de contactar o %title% e se quer uma solução honesta para os seus problemas, sejam grandes, graves ou antigos, contacte-o. Sigilo absoluto. Efectua trabalhos com %methods%. Faz trabalhos à distância. Consulta pessoalmente ou por cartas.",
+            "Experiência em todos os campos: %specialities%, %guarantees%. %problems%, %problems%. Conhecedor de todos os segredos e casos difíceis como: %problems%, %problems%, %problems%, %guarantees%, %guarantees%, %problems%, %guarantees%, %problems%, %problems%, %problems%, %problems%. %guarantees%. %timed_guarantees%. Se queres ter uma vida nova e deixar tudo o que te preocupa, fale com o %rank% %name%."
+        ],
+        "presentation_text": [
+            "Grande ilustre %specialities% %specialities% com %qualities%, %qualities%, %qualities%.",
+            "%specialities% - %specialities% - %specialities%"
         ],
         "methods": [
             "cadeado verde",
             "técnica simples",
             "aproximação",
             "afastamento rápido"
-        ]
-        "guarantee": ["Resultados máximos em 7 dias","Bons resultados"],
-        "schedule":['Consultas de Astrologia das 24h',"2ª a sábado das 09h às 21h"],
+        ],
+        "qualities": [
+            "rapidez",
+            "eficácia",
+            "garantia"
+        ],
+        // basic structure for generating a full spiritualist card
+        "templates": [
+            ["title", "subtitle", "presentation_text", "text", "schedule_data", "contacts"],
+            ["subtitle", "title", "text", "schedule_data", "contacts"]
+        ],
+        "timed_guarantees": [
+            "ajuda a resolver problemas em menos de 3 dias",
+            "resultados máximos em 7 dias",
+            "resultado garantido a 100% em pouco tempo"
+        ],
+        "guarantees": [
+            "atrair pessoas queridas",
+            "bons resultados",
+            "dá sorte nos exames",
+            "mantém-no no posto de trabalho",
+            "recupera a pessoa amada",
+            "satisfaz desejos",
+            "soluciona qualquer tipo de problema por mais difícil que seja",
+        ],
+        "schedule":[
+            "2ª a sábado das 09h às 21h",
+            'Consultas de Astrologia das 24h',
+        ],
         "address": ['Rua Coronel António dos Santos Fonseca, nº6, 3º Esq.º, Faro', 'Rua Dr. Coelho Carvalho, nº7 r/c, 8000 - FARO'], // will be deprecated by an address generator
-        "contact":["999 999 999"], // will be deprecated by a number generator
+        "contact":["999 999 999","222 222 222"], // will be deprecated by a number generator
         get_title: function() {
             // return a random rank and name and speciality in a format that is acceptable
         },
